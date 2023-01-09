e021b41d1c55a58f448308ab741e17d875129073
Ferraz <nacd@gft.com>
2023-01-09 10:57:19 -0300

    Erros de sintaxe

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -1,39 +1,39 @@
 describe('Scenarios where authentication is a pre-requirement', () => {
-  beforeEach(() => 
-    
-    cy.login() 
+  beforeEach(() =>
 
-    )
+    cy.login()
 
-    it('CRUDs a note', () => {
-      const faker = require('faker')
-      const noteDescription = faker.lorem.words(4)
-    
-      cy.intercept('GET', '**/notes').as('getNotes')
-    
-      cy.createNote(noteDescription)
-      cy.wait('@getNotes')
-    
-      const updatedNoteDescription = faker.lorem.words(4)
-      const attachFile = true
-    
-      cy.editNote(noteDescription, updatedNoteDescription, attachFile)
-      cy.wait('@getNotes')
-    
-      cy.deleteNote(updatedNoteDescription)
-      cy.wait(7000)
-      cy.wait('@getNotes')
-    })
-    it('successfully submits the form', () => {
-      cy.intercept('GET', '**/notes').as('getNotes')
-      cy.intercept('POST', '**/prod/billing').as('paymentRequest')
+  )
 
-      cy.fillSettingsFormAndSubmit()
-  
-      cy.wait(7000)
-      cy.wait('@getNotes')
-      cy.wait('@paymentRequest').then(response => {
-        expect(response.state).to.equal('Complete')
+  it('CRUDs a note', () => {
+    const faker = require('faker')
+    const noteDescription = faker.lorem.words(4)
+
+    cy.intercept('GET', '**/notes').as('getNotes')
+
+    cy.createNote(noteDescription)
+    cy.wait('@getNotes')
+
+    const updatedNoteDescription = faker.lorem.words(4)
+    const attachFile = true
+
+    cy.editNote(noteDescription, updatedNoteDescription, attachFile)
+    cy.wait('@getNotes')
+
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait(7000)
+    cy.wait('@getNotes')
+  })
+  it('successfully submits the form', () => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.intercept('POST', '**/prod/billing').as('paymentRequest')
+
+    cy.fillSettingsFormAndSubmit()
+
+    cy.wait(7000)
+    cy.wait('@getNotes')
+    cy.wait('@paymentRequest').then(response => {
+      expect(response.state).to.equal('Complete')
     })
   })
   it('logout', () => {
